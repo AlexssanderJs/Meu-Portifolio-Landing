5f8e68000ce77850593fea37acafd4416e9811c4
AlexssanderJs <josesenra11@gmail.com>
2025-09-15 21:42:21 -0300

    feat: adiciona header e rotas

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { PagesRoutingModule } from "../../pages/pages-routing-module";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PagesRoutingModule],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
