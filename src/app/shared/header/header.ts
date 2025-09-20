42fe20ae2e7f8ebcd6b34bb9043b2590a2f2fec8
AlexssanderJs <josesenra11@gmail.com>
2025-09-20 14:44:53 -0300

    refactor: integracao do header na home

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { PagesRoutingModule } from "../../pages/pages-routing-module";
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-header',
-  imports: [PagesRoutingModule],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
