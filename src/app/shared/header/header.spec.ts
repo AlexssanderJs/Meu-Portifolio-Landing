d848a2085a6b7d990ad98c18354ec855350b3a6e
AlexssanderJs <josesenra11@gmail.com>
2025-09-28 13:51:40 -0300

    Adiciona Jest

--- src/app/shared/header/header.spec.ts
+++ src/app/shared/header/header.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Header } from './header';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('Header', () => {
   let component: Header;
@@ -8,7 +9,7 @@ describe('Header', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Header]
+      imports: [Header, RouterTestingModule]
     })
     .compileComponents();
 
